c8d3a540aca72b4a6b80b0eeebb27e474838d85c
G UTHRA <uthra.156@gmail.com>
2021-12-02 23:42:38 +0530

    Update payment.js

--- scripts/payment.js
+++ scripts/payment.js
@@ -1,1 +1,11 @@
+let enableOrDisablePayNowButton = () => {
+    let isUserLoggedIn = localStorage.getItem("isUserLoggedIn");
+    let payNowButton = document.getElementById("payNowBtn");
+    if (isUserLoggedIn && isUserLoggedIn === "false") {
+      payNowButton.disabled = true;
+    } else if (isUserLoggedIn === "true") {
+      payNowButton.disabled = false;
+    }
+};
 
+enableOrDisablePayNowButton();
